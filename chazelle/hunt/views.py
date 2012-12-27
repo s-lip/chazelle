fbed7f243ce31af7c38a94fd6a3e47517f8bc393
Asheesh Laroia <asheesh@asheesh.org>
2012-12-26 22:38:36 -0500

    Add untested cookie check

--- chazelle/hunt/views.py
+++ chazelle/hunt/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 import hunt.hunt_state
 import os.path
 import django.http
+import urllib
 
 def get_url_with_cache(u):
     import httplib2 # optional dependency
     h = httplib2.Http(os.path.expanduser("~/.httplibcache/"))
     resp, content = h.request(u, "GET")
     return content
+
+def session_id_looks_good(cookie_dict):
+    import httplib2 # optional dependency
+    url = "http://z.manicsages.org/puzzle/login.php"
+
+    cookie_dict_as_string = urllib.urlencode(cookie_dict)
+    h = httplib2.Http(os.path.expanduser("~/.httplibcache/"))
+    resp, content = h.request(url, "GET",
+                              headers={'Cookie': cookie_dict_as_string})
+    return ("You are logged in. Would you like to" in content)
 
 def home(request):
     context = hunt.hunt_state.HuntState().get_context(
@@ -29,6 +40,8 @@ def note(request, note_name):
 
 def postprod(request):
     puzzle_html = get_url_with_cache(request.GET['htmlurl'])
-    if not request.GET['sekrit'] == 'supersekrit':
-        assert request.COOKIES['PHPSESSID'] # basic for now
+    if request.GET['sekrit'] == 'supersekrit':
+        pass # yay back door
+    else:
+        assert session_id_looks_good(request.COOKIE)
     return django.http.HttpResponse(puzzle_html)
