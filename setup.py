daec660ba6c3771111828a00bb15ef923edf08ab
Jason Bernardino Alonso <jalonso@lumino.so>
2012-12-30 16:11:23 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+VERSION = "0.1"
+
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="chazelle",
+    version = VERSION,
+    url = 'http://github.com/mysteryhunt/chazelle',
+    platforms = ["any"],
+    description = "MIT Mystery Hunt 2013",
+    ext_modules = [],
+    packages=find_packages(exclude=["tests"]),
+    install_requires=['veil'],
+)
