239d02cce320874456ffe1f6ff48cea48aa5c753
Jason Bernardino Alonso <jalonso@lumino.so>
2013-01-03 22:37:21 -0500

    Removing the old chazelle testing framework

--- chazelle/rounds/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-optional = dict(blank=True, null=True)
-
-# We do NOT store any metadata about rounds or puzzles in the database as far as this app is concerned.
-
-
--- chazelle/rounds/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    url(r'^(?P<round_slug>[\w_]+)/$', 'rounds.views.round', name='round'),
-    url(r'^(?P<round_slug>[\w_]+)/(?P<puzzle_slug>[\w_]+)/$', 'rounds.views.puzzle', name='puzzle'),
-)
