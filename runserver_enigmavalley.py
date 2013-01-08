1b753bbb5540afb57dd9e4d5a8079feaa2947e7b
Asheesh Laroia <asheesh@asheesh.org>
2013-01-08 18:31:02 -0500

    Creating an enigmavalley.com runserver

---
+++ runserver_enigmavalley.py
@@ -0,0 +1,19 @@
+import sys
+import os.path
+
+sys.path.append('../veil')
+import veil.cli.testing
+
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append('chazelle')
+    import vendor
+    vendor.vendorify()
+
+    sys.argv = ['./bin/veil-test-server', '-L', 'debug', '-P', '3001',
+                '--secret-key', 'plain', 'radically ephemeral flimsy kumquats',
+                'test.json',
+                os.path.abspath('./assets/')]
+    veil.cli.testing.run_server()
