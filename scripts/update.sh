a3888462bb8c10bd00d9d618587975b5e4be04cf
System User <jenkins@turin.int.enigmavalley.com>
2013-01-11 20:50:40 -0500

    Auto-commit 2013-0111-2050

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,7 +4,7 @@ umask 0002
 DATE=`date '+%Y-%m%d-%H%M'`
 git fetch origin master
 git add -A
-git commit -m "Auto-commit $DATE" || (echo skipping commit; exit 0)
+git commit -m "Auto-commit $DATE" || echo skipping commit && exit 0
 echo mergebase=$(git merge-base FETCH_HEAD master)
 mergebase=$(git merge-base FETCH_HEAD master)
 echo git merge-tree $mergebase master FETCH_HEAD | grep -qv 'changed in both'
