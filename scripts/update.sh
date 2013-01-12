e325f61ee5ac37a97a15a1aff493286a46b01f4d
Jason Bernardino Alonso <jalonso@lumino.so>
2013-01-11 20:32:22 -0500

    More aggressive auto-committing

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
+echo umask 0002
+umask 0002
 DATE=`date '+%Y-%m%d-%H%M'`
+echo DATE=$DATE
+echo git add -A
 git add -A
+echo git commit -m "Auto-commit $DATE"
 git commit -m "Auto-commit $DATE"
 echo git fetch origin master
 git fetch origin master
@@ -11,3 +16,5 @@ echo git merge-tree $mergebase master FETCH_HEAD | grep -qv 'changed in both'
 git merge-tree $mergebase master FETCH_HEAD | grep -qv 'changed in both'
 echo git rebase FETCH_HEAD
 git rebase FETCH_HEAD
+echo git push origin master
+git push origin master
